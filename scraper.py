ae15634f17d2bb2111c976d2a088d86bbe913ba5
dboudro <dylanrboudro@gmail.com>
2016-10-19 18:04:49 -0400

    update scraper

--- scraper.py
+++ scraper.py
@@ -1,22 +1,39 @@
 import wikipedia
-print " "
-print "100 plant searches"
-print wikipedia.search("Plants", results=100)
-print " "
-print "Lavender summary"
-print wikipedia.summary("lavender")
-print " "
+
 print "Oregano Wikipedia Page Object"
+
+wikipedia.search('plants in North America')
+
+
 oregano = wikipedia.WikipediaPage("Oregano")
+oregano = wikipedia.WikipediaPage("test")
 
-print oregano
-print " "
-print "Oregano categories"
-print oregano.categories
-print " "
-print "Oregano Medical Uses"
-print oregano.section("Medical Uses")
+oregano.html()
 
-print " "
-print "Oregano object and properties"
-print (vars(oregano))
+
+
+
+# print " "
+# print "100 plant searches"
+# print wikipedia.search("Plants", results=100)
+# print " "
+# print "Lavender summary"
+# print wikipedia.summary("lavender")
+# print " "
+
+# print oregano
+# print " "
+# print "Oregano categories"
+# print oregano.categories
+# print " "
+# print "Oregano Sections"
+# print oregano.sections
+
+
+
+
+# print " "
+# print " "
+# print " "
+# print "Oregano full html page"
+# print oregano.html()
