f16920812b5fcc3812e7b475bcdd4575c15ff730
dboudro <dylanrboudro@gmail.com>
2016-11-19 14:21:12 -0500

    patch 328

--- scraper.py
+++ scraper.py
@@ -19,7 +19,7 @@ for li in items:
 
 
 def loadPage(plant):
-    if plant == 'Bursaria' or plant == 'Carnegiea' or plant == 'Hohenbergiopsis' or plant == 'Ledum' or plant == 'Rochea' or plant == 'Sollya' or plant == 'Wigginsia':
+    if plant == 'Bursaria' or plant == 'Carnegiea' or plant == 'Hohenbergiopsis' or plant == 'Ledum' or plant == 'Rochea' or plant == 'Sollya' or plant == 'Wigginsia' or plant == 'Borago':
         print 'skipped'
         return 'skipped'
     else:
